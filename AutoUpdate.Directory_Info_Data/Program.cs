e4a33c3482e951ae4e15d09535d11c4a6db22c16
larry.lai <larry.lai@advantech.com>
2021-08-11 14:22:07 +0800

    自動產出檔案結構資料部分新增直接產出更新用壓縮檔功能

--- AutoUpdate.Directory_Info_Data/Program.cs
+++ AutoUpdate.Directory_Info_Data/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Text;
+using System.Diagnostics;
 
 using Newtonsoft.Json;
+using Ionic.Zip;
 
 using AutoUpdate.modules;
 
@@ -11,16 +14,29 @@ namespace AutoUpdate.Directory_Info_Data
     {
         static void Main(string[] args)
         {
-            string path = path = Environment.CurrentDirectory;
-            if (args.Length > 0)
-            {
-                path = args[0];
-            }
+            string path = args.Length > 0 ? args[0] : Environment.CurrentDirectory;
 
+            #region AutoUpdate.Directory_Info_Data
             Directory_Info directory_Info = new Directory_Info(path);
             string json = JsonConvert.SerializeObject(directory_Info);
+            File.WriteAllText(Path.Combine(path ,"AutoUpdate.Directory_Info_Data.json"), json);
+            #endregion
 
-            File.WriteAllText(path + "AutoUpdate.Directory_Info_Data.json", json);
+
+
+            #region Save Zip File
+            string TargetFileName = File.ReadAllText("TargetFileName").Trim();
+            FileVersionInfo fileVersion = FileVersionInfo.GetVersionInfo(TargetFileName);
+            FileInfo fileInfo = new FileInfo(TargetFileName);
+            string zipFileName = fileInfo.Name.Substring(0, fileInfo.Name.Length - fileInfo.Extension.Length) + fileVersion.FileVersion + ".zip";
+
+            using ZipFile zip = new ZipFile(Encoding.Default); //Encoding.Default 處理中文問題
+            zip.AddDirectory(path);
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            string PublishPath = directoryInfo.Parent.FullName + "\\" + directoryInfo.Name + "_Publish\\";
+            if(!Directory.Exists(PublishPath)) Directory.CreateDirectory(PublishPath);
+            zip.Save(PublishPath + zipFileName);
+            #endregion
         }
     }
 }
